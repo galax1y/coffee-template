d23ca6392e3a85e5982e4f59eb14f5180bb1aeff
galax1y <lucasvw.eng@gmail.com>
2024-02-18 20:11:01 -0300

    Add: Project template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+// Vite + React + TailwindCSS
+
+export function App() {
+  return (
+    <main className='container flex items-center justify-center mx-auto bg-red-500'>
+      <div>Hello</div>
+      <div>World!</div>
+    </main>
+  )
+}
